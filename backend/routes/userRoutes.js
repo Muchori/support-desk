d13784951899eb73900a45d1f155b5aa8e53df55
Muchori <muchorijoseph97@gmail.com>
2022-06-11 12:19:45 +0300

    Frontend Desgin-Login, Home and Registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
 
 /**
- * Register router
+ * Register route
  */
 router.post('/', registerUser)
 
 /**
- * Login router
+ * Login route
  */
 router.post('/login', loginUser)
+
+/**
+ * Me route
+ */
+router.get('/me', protect, getMe)
 
 module.exports = router
