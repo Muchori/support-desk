e43beefd90a770b260ed615f7ced8004846de31b
Muchori <muchorijoseph97@gmail.com>
2022-06-12 23:23:20 +0300

    Fix error in deployment

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
-const e = require('express')
 
 const protect = asyncHandler(async (req, res, next) => {
   let token
@@ -11,23 +10,24 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      // get token from header
+      // Get token from header
       token = req.headers.authorization.split(' ')[1]
-      //verify token
+      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      //get user from tojen
+      // Get user from token
       req.user = await User.findById(decoded.id).select('-password')
+
       next()
     } catch (error) {
       console.log(error)
       res.status(401)
-      throw new Error('Not Authorized')
+      throw new Error('Not authorized')
     }
   }
 
   if (!token) {
     res.status(401)
-    throw new Error('Not Authorized')
+    throw new Error('Not authorized')
   }
 })
 
